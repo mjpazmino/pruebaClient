a3405f416fcabe33197a4ee807ab0951cc103299
María José Pazmiño <65615849+mjpazmino@users.noreply.github.com>
2022-07-13 19:06:31 -0500

    first commit

---
+++ src/main/java/ec/edu/espe/pruebaclient/PruebaClientApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.pruebaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/edu/espe/pruebaclient/PruebaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.pruebaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
